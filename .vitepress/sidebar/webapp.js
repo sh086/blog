db9efea74e5d0aa0243eab1eeacd60aa1015a861
suhe <suheforvip@gmail.com>
2025-03-04 00:44:16 +0800

    add:新增vitpress部署方式

--- .vitepress/sidebar/webapp.js
+++ .vitepress/sidebar/webapp.js
@@ -3,6 +3,7 @@ const vitepress =  [
     text: '指南',
     collapsed: false,
     items: [
+      // link需要使用全路径,不能使用相对路径 否则侧边栏选中项变成蓝色 这一效果会失效
       { text: '快速开始', link: '/docs/guide/develop/webapp/vitepress/'},
       { text: '站点配置', link: '/docs/guide/develop/webapp/vitepress/01_站点配置'},
       { text: '部署', link: '/docs/guide/develop/webapp/vitepress/02_部署'},
@@ -12,8 +13,8 @@ const vitepress =  [
     text: '写作',
     collapsed: false, 
     items: [
-      { text: '导航栏', link: ''},
-      { text: '侧边栏', link: ''},
+      { text: '导航栏', link: '/docs/guide/develop/webapp/vitepress/03_导航栏'},
+      { text: '侧边栏', link: '/docs/guide/develop/webapp/vitepress/04_侧边栏'},
       { text: 'MarkDown拓展', link: ''},
       { text: 'frontmatter', link: ''},
       { text: '在MarkDown使用Vue', link: ''},
