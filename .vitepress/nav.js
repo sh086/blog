4dd1adfed69e91cb1b2d7441208c4d64fdd3f5a5
suhe <suheforvip@gmail.com>
2026-02-23 23:00:47 +0800

    1

--- .vitepress/nav.js
+++ .vitepress/nav.js
@@ -1,14 +1,14 @@
 export default [
-  {
-    text: '指南',
-    items: [
-      { text: '互联网全栈工程师', link: '/docs/guide/webapp/' },
-      { text: '人工智能及应用', link: '/docs/guide/chatgpt/' },
-      { text: '面试指南', link: '/docs/guide/interview/' },
-    ]
-  },
+  // {
+  //   text: '指南',
+  //   items: [
+  //     { text: '互联网全栈工程师', link: '/docs/guide/webapp/' },
+  //     { text: '人工智能及应用', link: '/docs/guide/chatgpt/' },
+  //     { text: '面试指南', link: '/docs/guide/interview/' },
+  //   ]
+  // },
   {text: '开发', link: '/docs/develop/'},
   {text: '工作流', link: '/docs/workflow/'},
   {text: '量化', link: '/docs/quant/'},
-  // {text: '创业', link: ''},
+  {text: '创业', link: '/docs/project/'},
 ] 
